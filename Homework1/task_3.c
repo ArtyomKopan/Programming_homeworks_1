b907e0fe91caf1748eb8272c4bfaef6e0fe16e70
ArtyomKopan <artem.kopan.2003@mail.ru>
2021-09-22 14:51:07 +0300

    I added a solution to the third problem

--- Homework1/task_3.c
+++ Homework1/task_3.c
@@ -1,6 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void swapBeginningWithEnd(int* array, int m, int n)
+{
+    for (int k = 0; k < m; ++k) {
+        int temp = array[0];
+        for (int i = 0; i < m + n - 1; ++i)
+            array[i] = array[i + 1];
+        array[m + n - 1] = temp;
+    }
+}
+
 int main()
 {
     int m = 0, n = 0;
@@ -12,42 +22,8 @@ int main()
     printf("Enter the array elements separated by a space: ");
     for (int i = 0; i < m + n; ++i)
         scanf("%i", &array[i]);
-
-    for (int k = 0; k < m; ++k) {
-        int temp = array[0];
-        for (int i = 0; i < m + n - 1; ++i)
-            array[i] = array[i + 1];
-        array[m + n - 1] = temp;
-    }
-
-    /*
-    if (m == n)
-        for (int i = 0; i < m; ++i) {
-            int temp = array[i];
-            array[i] = array[m + i];
-            array[m + i] = temp;
-        }
-    else if (m < n){
-        for (int i = 0; i < m; ++i) {
-            int temp = array[i];
-            array[i] = array[m + i];
-            array[m + i] = temp;
-        }
-        for (int i = 0; i < n - m; ++i) {
-            int temp = array[m + i];
-            array[m + i] = array[m + m + i];
-            array[m + m + i] = temp;
-        }
-        for (int i = n, j = 2 * m; i < 2 * m, j < n + m; ++i, ++j) {
-            int temp = array[i];
-            array[i] = array[j];
-            array[j] = temp;
-        }
-    }
-    else {
-
-    }
-    */
+    
+    swapBeginningWithEnd(array, m, n);
 
     for (int i = 0; i < m + n; ++i)
         printf("%i ", array[i]);
