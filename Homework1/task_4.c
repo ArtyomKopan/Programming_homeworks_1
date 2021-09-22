672f276bfdc59d7a6fe12932882b808775781e59
ArtyomKopan <artem.kopan.2003@mail.ru>
2021-09-22 14:57:58 +0300

    I corrected a solution to the fourth problem

--- Homework1/task_4.c
+++ Homework1/task_4.c
@@ -1,11 +1,12 @@
+#include <stdbool.h>
 #include <stdio.h>
 
-int isPrime(int x)
+bool isPrime(int x)
 {
     for (int probe = 2; probe < x; ++probe)
         if (x % probe == 0)
-            return 0;
-    return 1;
+            return false;
+    return true;
 }
 
 int main()
@@ -13,9 +14,13 @@ int main()
     printf("Enter the upper limit of the range of prime numbers:");
     int border = 0;
     scanf("%i", &border);
-    printf("Prime numbers not exceeding border:\n");
-    for (int x = 2; x <= border; ++x)
-        if (isPrime(x))
-            printf("%i ", x);
+    if (border <= 1)
+        printf("There are no primes in this range");
+    else {
+        printf("Prime numbers not exceeding border:\n");
+        for (int x = 2; x <= border; ++x)
+            if (isPrime(x))
+                printf("%i ", x);
+    }
     return 0;
 }
